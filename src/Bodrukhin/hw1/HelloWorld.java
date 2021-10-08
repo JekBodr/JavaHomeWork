098b1a50b5ac33b07797fb62bb29b096807989c9
JekBodr <jekbodr@gmail.com>
2021-10-08 20:21:19 +0300

    *

--- src/Bodrukhin/hw1/HelloWorld.java
+++ src/Bodrukhin/hw1/HelloWorld.java
@@ -1,11 +1,10 @@
-package Bodrukhin.hw1;
-/*
+package Bodrukhin.hw1;/*
  *
  *@Author Bodrukhin Ievgen
  * version 1.0.0
  */
- class HelloWorld {
 
+public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
---
+++ src/Bodrukhin/hw1/Trainy.java
@@ -0,0 +1,8 @@
+package Bodrukhin.hw1;/*
+ *
+ *@Author Bodrukhin Ievgen
+ * version 1.0.0
+ */
+
+public class Trainy {
+}
