3aee0781abec35da22fc85ba97750f03c04e0cb3
JekBodr <90924136+JekBodr@users.noreply.github.com>
2021-12-22 09:48:45 +0200

    Add files via upload

---
+++ src/Bodrukhin/hw12/Main.java
@@ -0,0 +1,82 @@
+package hw12;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        PhoneBook phoneBook = new PhoneBook();
+        Scanner scan = new Scanner(System.in);
+        System.out.println("""
+                Добавить контакт нажимайте   1
+                Найти контакт нажимайте 2
+                Удалить контакт нажимайте  3
+                Просмотреть контакт нажимайте 4
+                Сортировать контакт по ФИО нажимайте 5
+                Сортировать контакт по дате рождения нажимайте 6
+                Редактировать контакт  нажимайте 7
+                Вывести  список в JSON нажимайте 8
+                Записать в книгу в JSON нажимайте 9
+                Для выхода нажимайте 0
+                """);
+        int userData;
+        userData = scan.nextInt();
+        boolean run = true;
+        while (run) {
+            switch (userData) {
+                case 1 -> {
+                    phoneBook.Add();
+                    System.out.println("Введите новый запрос: ");
+                    userData = Integer.parseInt(scan.nextLine());
+
+                }
+                case 2 -> {
+                    System.out.println(phoneBook.Search());
+                    System.out.println("Введите новый запрос: ");
+                    userData = Integer.parseInt(scan.nextLine());
+                }
+                case 3 -> {
+                    phoneBook.Remove();
+                    System.out.println("Введите новый запрос: ");
+                    userData = Integer.parseInt(scan.nextLine());
+                }
+                case 4 -> {
+                    phoneBook.Modify();
+                    System.out.println("Введите новый запрос: ");
+                    userData = Integer.parseInt(scan.nextLine());
+                }
+                case 5 -> {
+                    System.out.println(phoneBook.showPhoneBook());
+                    System.out.println("Введите новый запрос: ");
+                    userData = Integer.parseInt(scan.nextLine());
+                }
+                case 6 -> {
+                    System.out.println(phoneBook.SortByName());
+                    System.out.println("Введите новый запрос: ");
+                    userData = Integer.parseInt(scan.nextLine());
+                }
+                case 7 -> {
+                    System.out.println(phoneBook.SortByDateOfBirth());
+                    System.out.println("Введите новый запрос: ");
+                    userData = Integer.parseInt(scan.nextLine());
+                }
+                case 8 -> {
+                    phoneBook.toFile();
+                    System.out.println("Введите новый запрос: ");
+                    userData = Integer.parseInt(scan.nextLine());
+                }
+                case 9 -> {
+                    phoneBook.fromFile();
+                    userData = Integer.parseInt(scan.nextLine());
+                }
+                case 0 -> {
+                    run = false;
+                    System.out.println("До встречи!");
+                }
+                default -> {
+                    System.out.println("Неверный запрос. Попробуйте снова.");
+                    userData = Integer.parseInt(scan.nextLine());
+                }
+            }
+        }
+    }
+}
---
+++ src/Bodrukhin/hw12/Person.java
@@ -0,0 +1,80 @@
+package hw12;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Arrays;
+
+public class Person {
+    private String name;
+    private String address;
+    private String[] phones;
+    private transient LocalDate dateOfBirth;
+    private transient LocalDateTime modified;
+
+    public Person(String fullName, String address, String[] phones,
+                  LocalDate dateOfBirth, LocalDateTime modified) {
+        this.name = fullName;
+        this.address = address;
+        this.phones = phones;
+        this.dateOfBirth = dateOfBirth;
+        this.modified = modified;
+    }
+
+    public Person() {
+
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setPhones(String[] phones) {
+        this.phones = phones;
+    }
+
+    public void setDateOfBirth(int y, int m, int d) {
+        this.dateOfBirth = LocalDate.of(y, m, d);
+    }
+
+    public void setModified(LocalDateTime modified) {
+        this.modified = modified;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void getName(String newName) {
+    }
+
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String[] getPhones() {
+        return phones;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth.toString();
+    }
+
+    public String getModified() {
+        return modified.toString();
+    }
+
+    @Override
+    public String toString() {
+        return "\n{ФИО: " + name + "\n" +
+                "Адрес: " + address + "\n" +
+                "Дата рождения: " + dateOfBirth + "\n" +
+                "Телефоны: " + Arrays.toString(phones) + "\n" +
+                "Время изменения: " + modified + "}";
+    }
+
+
+}
